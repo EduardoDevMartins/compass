80f91858c0568e635b83adcdce4054f1c18796ef
Eduardo Martins <eduardodevmartins@gmail.com>
2024-10-20 17:40:53 -0300

    Foi incluído uma autoomação a cada 5 minutos para verificar o status

--- script.py
+++ script.py
@@ -1,30 +1,43 @@
+#!/usr/bin/env python3
 import os
 import datetime
 import subprocess
+import time
 
-# Folder where I put the Results
+# Locais dos diretórios de logs
 LOG_DIR = "/home/eduardo/Área de trabalho/atividade1compass/logs"
 ONLINE_LOG = os.path.join(LOG_DIR, "nginx_online.log")
 OFFLINE_LOG = os.path.join(LOG_DIR, "nginx_offline.log")
 
-# DATE And TIME
-current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+# Função para verificar o status do serviço e permitir o loop a cada 5 min
+def check_nginx_status():
+    # Data e Hora atual
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-#Validate the status of service
-service = "nginx"
-status_command = ["systemctl", "is-active", service]
+    # Verifica o status do serviço Nginx
+    service = "nginx"
+    status_command = ["systemctl", "is-active", service]
 
-try:
-    status_output = subprocess.check_output(status_command).strip().decode('utf-8')
+    try:
+        # Executa o comando para verificar o status do Nginx
+        status_output = subprocess.check_output(status_command).strip().decode('utf-8')
 
-    if status_output == "active":
-        
-        with open(ONLINE_LOG, "a") as online_file:
-            online_file.write(f"{current_time} - {service} - ONLINE - O serviço está rodando\n")
-    else:
+        if status_output == "active":
+            # Serviço está rodando
+            with open(ONLINE_LOG, "a") as online_file:
+                online_file.write(f"{current_time} - {service} - ONLINE - O serviço está rodando\n")
+        else:
+            # Serviço não está rodando
+            with open(OFFLINE_LOG, "a") as offline_file:
+                offline_file.write(f"{current_time} - {service} - OFFLINE - O serviço não está rodando\n")
+
+    except subprocess.CalledProcessError:
+        # Caso haja algum erro ao executar o comando
         with open(OFFLINE_LOG, "a") as offline_file:
-            offline_file.write(f"{current_time} - {service} - OFFLINE - O serviço não está rodando\n")
+            offline_file.write(f"{current_time} - {service} - OFFLINE - Erro ao verificar o status do serviço\n")
 
-except subprocess.CalledProcessError:
-    with open(OFFLINE_LOG, "a") as offline_file:
-        offline_file.write(f"{current_time} - {service} - OFFLINE - Erro ao verificar o status do serviço\n")
+# Loop infinito para executar a cada 5 minutos
+while True:
+    check_nginx_status()
+    # Aguarda 300 segundos (5 minutos)
+    time.sleep(300)
